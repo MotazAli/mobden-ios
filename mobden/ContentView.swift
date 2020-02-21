49e3b465bca57d06ea556b28092a9f844ccfef98
motaz <motaz_ali@hotmail.com>
2020-02-20 21:55:20 -0800

    Initial Commit

---
+++ mobden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mobden
+//
+//  Created by administrator on 2/20/20.
+//  Copyright © 2020 motaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
