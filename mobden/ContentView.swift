0b4790dfbbd47cd7607fc1a8837201a6075b6afb
motaz <motaz_ali@hotmail.com>
2020-02-26 07:15:06 -0800

    bass project

--- mobden/ContentView.swift
+++ mobden/ContentView.swift
@@ -6,13 +6,277 @@
 //  Copyright © 2020 motaz. All rights reserved.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    
+   
+    @State var masterSilderOpen = false
+    @State var highSchoolSliderOpen = false
+    @State var middleSchoolSliderOpen = false
+    @State var primarySchoolSliderOpen = false
+    @State var selectedTabView = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        
+        
+        ZStack{
+        
+            
+            VStack{
+                
+                HStack{
+                    Spacer()
+                    Text(" طلائع المبدعين الاهلية للبنين").font(.title).lineLimit(nil)
+                   
+                    Button(action: {
+                        
+                        
+                        if self.selectedTabView == 0{
+                            self.masterSilderOpen.toggle()
+                            
+                        }
+                        
+                        if self.selectedTabView == 1{
+                            self.primarySchoolSliderOpen.toggle()
+                            
+                        }
+                        
+                    })
+                    {
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                            .foregroundColor(.black)
+                    }
+                    
+                }
+                
+                //Spacer()
+                
+                
+                TabView(selection: self.$selectedTabView){
+                    
+                    Text("الثانوي").tabItem{
+                        Text("الثانوي")
+                        
+                    }.tag(3)
+                    
+                    
+                    Text("المتوسط").tabItem{
+                        Text("المتوسط")
+                        
+                    }.tag(2)
+                    
+                    
+                    
+                    VStack{
+                    
+                        Text("الابتدائي")                  }.tabItem{
+                        Text("الابتدائي")
+                        
+                        
+                        
+                    }.tag(1)
+                    
+                    NavigationView{
+                        
+                        List{
+                        
+                        
+                        VStack(alignment:.trailing){
+                            Text("")
+                            Image("company_logo")
+                            .resizable()
+                                .frame(width: 300, height: 200, alignment: .center)
+                            .scaledToFit()
+                            Text("")
+                            
+                            Spacer()
+                            
+                        }
+
+                            VStack(alignment:.trailing){
+                                    Text("مقالات")
+                                ScrollView(.horizontal,showsIndicators: true){
+                            
+                                    VStack(alignment:.leading){
+                                
+                                HStack{
+                                    ArticalsMainGroupView()
+                                   ArticalsMainGroupView()
+                                    ArticalsMainGroupView()
+                                    
+                                        }
+                                
+                            }
+                            
+                        }.frame(height:300)
+                        
+                            }
+                            
+                            
+                            
+                            VStack(alignment:.trailing){
+                                        Text("لوحة الشرف")
+                                    ScrollView(.horizontal,showsIndicators: true){
+                                
+                                        VStack(alignment:.leading){
+                                    
+                                    HStack{
+                                        ArticalsMainGroupView()
+                                       ArticalsMainGroupView()
+                                        ArticalsMainGroupView()
+                                        
+                                            }
+                                    
+                                }
+                                
+                            }.frame(height:300)
+                            
+                                }
+                            
+                            
+                            
+                            VStack(alignment:.trailing){
+                                        Text("عن المدرسة")
+                                    ScrollView(.horizontal,showsIndicators: true){
+                                
+                                        VStack(alignment:.leading){
+                                    
+                                    HStack{
+                                        Text("test")
+                                        
+                                            }
+                                    
+                                }
+                                
+                            }.frame(height:300)
+                            
+                                }
+                            
+                        
+                    }
+                        
+                        
+                    
+                        .navigationBarTitle(Text(""))
+                        
+                    }.tabItem{
+                        Text("الرئيسية")
+                        
+                    }.tag(0)
+                    
+                }
+                
+            }.padding()
+            
+            
+        
+            
+            
+            MasterMenuSlider(masterSilderOpen: self.$masterSilderOpen)
+            
+            PrimarySchoolSliderMenu(primarySchoolSliderOpen: self.$primarySchoolSliderOpen)
+            
+        }
+       
+        
+    }
+    
+
+}
+
+
+struct ArticalsMainGroupView :View{
+    var body:some View{
+        
+        
+         VStack(alignment:.center){
+                                               
+                                               Image("company_logo").resizable().frame(width: 300, height: 200, alignment: .center).scaledToFit();                                        Text("fdgsdfdbrghfghfghyjdfhdhdhgdfghsdfgsdbghgnfgbdfsdfhggbsdbfsdbsgjfndfnsdbsdnhdnsfgbs")
+                                               
+                                           }.frame(width: 300, height: 400, alignment: .center)
     }
 }
+
+
+
+struct LoginView: View {
+    
+    @State var authenticated: Bool = false
+    @State var username: String = ""
+    @State var password: String = ""
+    let lightGreyColor = Color(red: 210.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 6.0)
+    
+    
+    
+    
+     var body: some View {
+        
+                    VStack {
+                        
+                       
+                        
+                       
+                        
+                        Image("company_logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 300, height: 200)                    .clipped()
+                        .cornerRadius(150)
+                        .padding(.bottom, 20)
+                        
+                        
+                         Text("مدارس طلائع المبدعين")
+                                               .font(.largeTitle)
+                                           .fontWeight(.semibold)
+                                               .padding(.bottom, 20)
+                            
+                        
+                        TextField("اسم المستخدم", text: $username)
+                                        .padding()
+                                        .background(lightGreyColor)
+                                        .cornerRadius(5.0)
+                          .padding(.bottom, 20)
+                        SecureField("كلمه المرور", text: $password)
+                                        .padding()
+                                        .background(lightGreyColor)
+                                        .cornerRadius(5.0)
+                                        .padding(.bottom, 20)
+                        
+                        
+                       Button(action: {
+                                                        }) {
+                                     Text("دخول")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(width: 220, height: 60)
+                                        .background(Color.blue)
+                                    .cornerRadius(15.0)
+                                    
+                                    
+                                 }
+
+                        
+                    }
+        .padding()
+    }
+}
+
+
+
+
+struct HomeView: View {
+     var body: some View {
+        Text("Welcome")
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ mobden/SliderMenu.swift
@@ -0,0 +1,104 @@
+//
+//  SliderMenu.swift
+//  mobden
+//
+//  Created by administrator on 2/20/20.
+//  Copyright © 2020 motaz. All rights reserved.
+//
+import  Foundation
+import SwiftUI
+
+
+
+
+struct MasterMenuSlider : View {
+ 
+    @Binding var masterSilderOpen: Bool
+    let width: CGFloat = 270
+    
+    
+    
+    
+    var body: some View{
+        
+        ZStack{
+         GeometryReader { geometry in
+                     
+            VStack(alignment: .trailing) {
+                                  
+                                  HStack{
+                                    Spacer()
+                                    
+                                   
+                                    
+                                      Text("الرئيسية")
+                                    .padding(20)
+                                        .font(.title)
+                                
+                                    
+                                      
+                                  }
+                                  HStack{
+                                       Text("القبول و التسجيل").padding(20)
+                                    .font(.title)
+                                   }
+                                  
+                                   HStack{
+                                       Text("الجوائز").padding(20)
+                                    .font(.title)
+                                   }
+                                  HStack{
+                                      Text("الجودة الشاملة").padding(20)
+                                    .font(.title)
+                                  }
+                                  HStack{
+                                      Text("الاشراف و التطوير المهني").padding(20)
+                                        .font(.system(size: 25))
+                                    .scaledToFit()
+                                  }
+                                  HStack{
+                                      Text("العلاقات العامة").padding(20)
+                                    .font(.title)
+                                  }
+                                  HStack{
+                                      Text("النقل المدرسي").padding(20)
+                                    .font(.title)
+                                  }
+                                  HStack{
+                                      Text("اتصل بنا").padding(20)
+                                    .font(.title)
+                                  }
+                                  
+                                
+                                  }
+                                  .frame(width: 270,height:geometry.size.height, alignment: .topLeading)
+                                  .background(Color.red)
+                                  .offset(x: 80)
+                                  
+                                  
+                  }
+                   
+                  .background(Color.gray.opacity(0.3))
+         .opacity(self.masterSilderOpen ? 0.8 : 0.0)
+                  .animation(Animation.easeIn.delay(0.25))
+            .edgesIgnoringSafeArea(.bottom)
+                  .onTapGesture {
+                      self.masterSilderOpen.toggle()
+                  }
+            
+        
+        }
+        
+    }
+    
+    
+}
+
+
+struct MasterMenuSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        MasterMenuSlider(masterSilderOpen: Binding.constant(true))
+    }
+}
+
+
